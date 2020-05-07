82c44c86366411024c9590adb2dace7b9bf8689b
Nicolas Hamparsomian <nhamparsomia@uolinc.com>
2020-05-07 17:25:13 -0300

    Implementação da criação de um empréstimo de livro junto com teste unitário

---
+++ src/main/java/com/nhamparsomia/libraryapi/api/resource/LoanController.java
@@ -0,0 +1,37 @@
+package com.nhamparsomia.libraryapi.api.resource;
+
+import com.nhamparsomia.libraryapi.api.dto.LoanDTO;
+import com.nhamparsomia.libraryapi.model.entity.Book;
+import com.nhamparsomia.libraryapi.model.entity.Loan;
+import com.nhamparsomia.libraryapi.service.BookService;
+import com.nhamparsomia.libraryapi.service.LoanService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/api/loans")
+@RequiredArgsConstructor
+public class LoanController {
+    private final LoanService service;
+    private final BookService bookService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Long create(@RequestBody LoanDTO dto) {
+        Book book = bookService.getBookByIsbn(dto.getIsbn()).get();
+
+        Loan entity = Loan.builder()
+                .book(book)
+                .customer(dto.getCustomer())
+                .loanDate(LocalDate.now())
+                .build();
+
+        entity = service.save(entity);
+        return entity.getId();
+    }
+}
---
+++ src/main/java/com/nhamparsomia/libraryapi/service/LoanService.java
@@ -0,0 +1,7 @@
+package com.nhamparsomia.libraryapi.service;
+
+import com.nhamparsomia.libraryapi.model.entity.Loan;
+
+public interface LoanService {
+    Loan save(Loan loan);
+}
