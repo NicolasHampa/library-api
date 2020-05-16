3e6fbf75d4ac49876f36cd9a0810d370a79dd65e
NicolasHampa <n_hampa@hotmail.com>
2020-05-16 20:33:52 -0300

    Integração com servidor da MailTrap para envio de emails para clientes com empréstimos em atraso

---
+++ src/main/java/com/nhamparsomia/libraryapi/service/impl/EmailServiceImpl.java
@@ -0,0 +1,14 @@
+package com.nhamparsomia.libraryapi.service.impl;
+
+import com.nhamparsomia.libraryapi.service.EmailService;
+
+import java.util.List;
+
+public class EmailServiceImpl implements EmailService {
+
+    @Override
+    public void sendMailToLateLoans(String message, List<String> mailList) {
+
+    }
+
+}
