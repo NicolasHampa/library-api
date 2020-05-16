1fc95945aefc0231db84e7011c416ba6669c0424
NicolasHampa <n_hampa@hotmail.com>
2020-05-16 20:58:27 -0300

    Implementação do EmailService para envio de aviso de empréstimos atrasados

--- src/main/java/com/nhamparsomia/libraryapi/service/impl/EmailServiceImpl.java
+++ src/main/java/com/nhamparsomia/libraryapi/service/impl/EmailServiceImpl.java
@@ -1,14 +1,38 @@
 package com.nhamparsomia.libraryapi.service.impl;
 
 import com.nhamparsomia.libraryapi.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
+
+    @Value("${application.mail.default.sender}")
+    private String sender;
+
+    private final JavaMailSender mailSender;
+
+    @Autowired
+    private SimpleMailMessage mailMessage;
 
     @Override
     public void sendMailToLateLoans(String message, List<String> mailList) {
 
+        String[] customersEmailsArray = mailList.toArray(new String[mailList.size()]);
+
+        mailMessage.setFrom(sender);
+        mailMessage.setSubject("Aviso Biblioteca FJ: Você possui mpréstimos em atraso!");
+        mailMessage.setText(message);
+        mailMessage.setTo(customersEmailsArray);
+
+        mailSender.send(mailMessage);
     }
 
 }
